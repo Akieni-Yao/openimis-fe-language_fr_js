7bee37fc4a12b09fbf4e40bcfff2c99cbe3e536b
tom hikari <tomprescott1565@gmail.com>
2025-07-14 15:54:01 +0100

    fix: correct import statement and formatting in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import messages_fr from "./translations/fr.json";
-import flatten from "flat";
+import { flatten } from "flat";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: 'fr', messages: flatten(messages_fr) }],
-}
+  translations: [{ key: "fr", messages: flatten(messages_fr) }],
+};
 
 export const LanguageFrModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
-}
+};
